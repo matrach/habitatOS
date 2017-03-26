2386d4f45a97fea51538501cd96067d621026ead
Matt Harasymczuk <matt@astrotech.io>
2017-03-26 13:49:27 +0200

    Static urls for heroku #time 10m

--- habitat/urls.py
+++ habitat/urls.py
@@ -1,10 +1,13 @@
+import os
+from django.conf import settings
 from django.conf.urls import url
 from django.conf.urls import include
 from django.contrib import admin
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     url(r'^grappelli/', include('grappelli.urls'), name='grappelli'),
     url(r'^notepad/', include('habitat.notepad.urls'), name='notepad'),
     url(r'^', admin.site.urls, name='admin'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
