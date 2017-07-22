7511c5bd21efc7add1ba4c33031b04bd24757fa3
Matt Harasymczuk <matt@astrotech.io>
2017-07-22 10:08:08 +0200

    Transifex config command #time 30m

--- habitat/_common/management/commands/transifex.py
+++ habitat/_common/management/commands/transifex.py
@@ -1,13 +1,34 @@
 from django.apps import apps
+from django.conf import settings
 from django.core.management.base import BaseCommand
+
+HEADER = """[main]
+host = https://www.transifex.com
+
+[habitatos.main]
+file_filter = habitat/locale/<lang>/LC_MESSAGES/django.po
+source_file = habitat/locale/en/LC_MESSAGES/django.po
+source_lang = en
+type = PO
+"""
+
+TEMPATE = """
+[habitatos.{name}]
+file_filter = {path}/locale/<lang>/LC_MESSAGES/django.po
+source_file = {path}/locale/en/LC_MESSAGES/django.po
+source_lang = en
+type = PO
+"""
 
 
 class Command(BaseCommand):
     help = 'Generates transifex config'
 
     def handle(self, *args, **options):
-        for app in apps.get_models():
-            print(dir(app))
+        self.stdout.write(HEADER)
 
-            if app.label.startswith('habitat'):
-                self.stdout.write(self.style.SUCCESS(f'{name}'))
+        for app in settings.INSTALLED_APPS:
+            if app.startswith('habitat'):
+                path = app.replace('.', '/')
+                name = app.split('.')[-1]
+                self.stdout.write(TEMPATE.format(**locals()))
