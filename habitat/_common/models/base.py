23fd320ca042c05405c13f954bcd16017c07fabe
Matt Harasymczuk <matt@astrotech.io>
2017-10-28 22:53:17 +0200

    Datetime description #time 2m

--- habitat/_common/models/base.py
+++ habitat/_common/models/base.py
@@ -4,12 +4,12 @@ from django.utils.translation import ugettext_lazy as _
 
 class HabitatModel(models.Model):
     created = models.DateTimeField(
-        verbose_name=_('Add Datetime'),
+        verbose_name=_('Add Datetime [UTC]'),
         db_index=True,
         auto_now_add=True)
 
     modified = models.DateTimeField(
-        verbose_name=_('Modified Datetime'),
+        verbose_name=_('Modified Datetime [UTC]'),
         db_index=True,
         auto_now=True)
 
